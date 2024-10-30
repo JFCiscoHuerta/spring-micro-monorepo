769512331645654a44aa08394363c1e487b2810e
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-30 13:31:53 -0600

    feat: implement CustomerService with operations

--- businessdomain/Customer/src/main/java/com/gklyphon/Customer/service/CustomerServiceImpl.java
+++ businessdomain/Customer/src/main/java/com/gklyphon/Customer/service/CustomerServiceImpl.java
@@ -1,9 +1,13 @@
 package com.gklyphon.Customer.service;
 
+import com.gklyphon.Customer.exception.custom.ElementNotFoundException;
 import com.gklyphon.Customer.model.entity.Customer;
 import com.gklyphon.Customer.repository.ICustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +16,38 @@ public class CustomerServiceImpl implements ICustomerService {
     private final ICustomerRepository customerRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Customer findCustomerById(Long id) {
-        return customerRepository.findById(id).orElseThrow();
+        return customerRepository.findById(id).orElseThrow(
+                ()-> new ElementNotFoundException("Customer with id: " + id +" not found"));
+    }
+
+    @Override
+    @Transactional
+    public Customer saveCustomer(Customer customer) {
+        customer.setCreateAt(LocalDateTime.now());
+        customer.setUpdateAt(LocalDateTime.now());
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    @Transactional
+    public Customer updateCustomer(Long id, Customer customer) {
+        Customer originalCustomer = findCustomerById(id);
+        originalCustomer.setName(customer.getName());
+        originalCustomer.setPhone(customer.getPhone());
+        originalCustomer.setCreateAt(LocalDateTime.now());
+        originalCustomer.setUpdateAt(LocalDateTime.now());
+        return customerRepository.save(originalCustomer);
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteCustomerById(Long id) {
+        if (!customerRepository.existsById(id)) {
+            return false;
+        }
+        customerRepository.deleteById(id);
+        return true;
     }
 }
