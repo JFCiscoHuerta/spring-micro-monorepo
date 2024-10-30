7f5d422085630c6c03b799715df3728273fbc5c2
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-30 13:35:41 -0600

    test: add test for  CustomerServiceIml

--- businessdomain/Customer/src/test/java/com/gklyphon/Customer/service/CustomerServiceImplTest.java
+++ businessdomain/Customer/src/test/java/com/gklyphon/Customer/service/CustomerServiceImplTest.java
@@ -1,14 +1,17 @@
 package com.gklyphon.Customer.service;
 
 import com.gklyphon.Customer.Data;
+import com.gklyphon.Customer.exception.custom.ElementNotFoundException;
 import com.gklyphon.Customer.model.entity.Customer;
 import com.gklyphon.Customer.repository.ICustomerRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataAccessException;
 
 import java.util.Optional;
 
@@ -39,6 +42,53 @@ class CustomerServiceImplTest {
     }
 
     @Test
-    void name() {
+    void findCustomerById_shouldThrowElementNotFoundException_whenIdDoNotExists() {
+        doThrow(ElementNotFoundException.class).when(customerRepository).findById(100L);
+        assertThrows(ElementNotFoundException.class, ()-> customerService.findCustomerById(100L));
+        verify(customerRepository, times(1)).findById(100L);
+    }
+
+    @Test
+    void saveCustomer_shouldReturnCustomer_whenSaveCustomerSuccessfully() {
+        when(customerRepository.save(Data.CUSTOMER)).thenReturn(Data.CUSTOMER);
+        Customer customer = customerService.saveCustomer(Data.CUSTOMER);
+        assertAll(()->{
+            assertNotNull(customer);
+            assertEquals(Data.CUSTOMER.getId(), customer.getId());
+        });
+        verify(customerRepository, times(1)).save(Data.CUSTOMER);
+    }
+
+    @Test
+    void updateCustomer_shouldReturnCustomer_whenUpdateCustomerSuccessfully() {
+        when(customerRepository.save(Data.CUSTOMER)).thenReturn(Data.CUSTOMER);
+        when(customerRepository.findById(1L)).thenReturn(Optional.of(Data.CUSTOMER));
+        Customer customer = customerService.updateCustomer(1L, Data.CUSTOMER);
+        assertAll(()->{
+            assertNotNull(customer);
+            assertEquals(Data.CUSTOMER.getId(), customer.getId());
+        });
+        verify(customerRepository, times(1)).save(Data.CUSTOMER);
+        verify(customerRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    @Disabled
+    void updatePassword_whenUpdatePasswordSuccessfully() {
+
+    }
+
+    @Test
+    @Disabled
+    void updateEmail_whenUpdateEmailSuccessfully() {
+
+    }
+
+    @Test
+    void deleteCustomer_shouldReturnTrue_whenDeleteCustomerByIdSuccessfully() {
+        when(customerRepository.existsById(1L)).thenReturn(true);
+        assertTrue(customerService.deleteCustomerById(1L));
+        verify(customerRepository, times(1)).deleteById(1L);
+        verify(customerRepository, times(1)).existsById(1L);
     }
 }
