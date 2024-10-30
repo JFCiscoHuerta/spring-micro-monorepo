52bf01ca49dbd0c5a1a37d8596d9441d810835c0
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-10-30 13:28:31 -0600

    feat: add operations to ICustomerService interface

--- businessdomain/Customer/src/main/java/com/gklyphon/Customer/service/ICustomerService.java
+++ businessdomain/Customer/src/main/java/com/gklyphon/Customer/service/ICustomerService.java
@@ -5,5 +5,7 @@ import com.gklyphon.Customer.model.entity.Customer;
 public interface ICustomerService {
 
     Customer findCustomerById(Long id);
-
+    Customer saveCustomer(Customer customer);
+    Customer updateCustomer(Long id, Customer customer);
+    boolean deleteCustomerById(Long id);
 }
